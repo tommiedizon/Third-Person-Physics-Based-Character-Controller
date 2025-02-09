d92c96c3adc8b547642a7b56b311b8df2f16b4f6
Thomas Dizon <thomas.dizon25@gmail.com>
2025-02-09 19:34:02 +1100

    Added simple player and camera movement using Cinemachine, CharacterController and Unity's new input system. Sensitivity constant (and Y inverted for now). Demo: https://drive.google.com/file/d/1OdgmBsba13sEJ3XnNb1D0vTC6IQ8H55_/view?usp=drive_link

--- Assets/_Scripts/GameInput.cs
+++ Assets/_Scripts/GameInput.cs
@@ -7,30 +7,16 @@ public class GameInput : MonoBehaviour {
 
     InputActionSystem inputActionSystem;
 
-    public class OnMovePerformedEventArgs : EventArgs {
-        public Vector2 normalizedMoveDir;
-    }
-
-    public event EventHandler<OnMovePerformedEventArgs> OnMovePerformed;
-    public event EventHandler OnMoveCanceled;
-
     private void Awake() {
         GameInputInstance = this;
     }
     private void Start() {
         inputActionSystem = new InputActionSystem();
         inputActionSystem.Player.Enable();
-        inputActionSystem.Player.Move.performed += Move_performed;
-        inputActionSystem.Player.Move.canceled += Move_canceled;
     }
 
-    private void Move_canceled(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
-        OnMoveCanceled?.Invoke(this, EventArgs.Empty);
+    public Vector2 GetMovementVectorNormalized() {
+        return inputActionSystem.Player.Move.ReadValue<Vector2>().normalized;
     }
 
-    private void Move_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
-        OnMovePerformed?.Invoke(this, new OnMovePerformedEventArgs {
-            normalizedMoveDir = inputActionSystem.Player.Move.ReadValue<Vector2>()
-        });   
-    }
 }
--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -1,49 +1,42 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    CharacterController characterController;
-    [SerializeField] Transform playerCamera;
-    Vector3 forwardDir;
-    Vector3 perpDir;
-    Vector3 moveDir;
+    CharacterController controller;
+    [SerializeField] CinemachineCamera cam;
+
+    // Player settings
+    [SerializeField] float speed = 6f;
+    [SerializeField] float rotateSpeed = 6f;
 
     private void Start() {
         Cursor.lockState = CursorLockMode.Locked;
-        characterController = GetComponent<CharacterController>();
-
-        GameInput.GameInputInstance.OnMovePerformed += GameInputInstance_OnMovePerformed;
-        GameInput.GameInputInstance.OnMoveCanceled += GameInputInstance_OnMoveCanceled;
+        controller = GetComponent<CharacterController>();
     }
 
     private void Update() {
-        characterController.SimpleMove(forwardDir);
-    }
-
-    // Event listeners
-    private void GameInputInstance_OnMovePerformed(object sender, GameInput.OnMovePerformedEventArgs e) {
-        HandleMovement(e.normalizedMoveDir);
-    }
-
-    private void GameInputInstance_OnMoveCanceled(object sender, System.EventArgs e) {
-        HandleMovement(Vector2.zero);
+        HandleMovement();
     }
 
     // Helper functions
-    private void HandleMovement(Vector2 rawPlayerInput) {
-        forwardDir = ComputeForwardDirection(playerCamera);
-        perpDir = ComputePerpDirection(forwardDir);
+    private void HandleMovement() {
+        Vector2 inputVector = GameInput.GameInputInstance.GetMovementVectorNormalized();
+        Vector3 forwardDir = ComputeForwardDirection();
+        Vector3 perpDir = ComputePerpDirection(forwardDir);
+        Vector3 moveDir = (inputVector.y * forwardDir - inputVector.x * perpDir).normalized;
 
-        moveDir = rawPlayerInput.x * forwardDir + rawPlayerInput.y * perpDir;
+        controller.SimpleMove(moveDir * speed);
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
 
-    private Vector3 ComputeForwardDirection(Transform playerCamera) {
-        Vector3 vect = transform.position - playerCamera.transform.position;
-        return new Vector3(vect.x, 0, vect.z);
+    private Vector3 ComputeForwardDirection() {
+        Vector3 vect = transform.position - cam.transform.position;
+        return new Vector3(vect.x, 0, vect.z).normalized;
     }
 
     private Vector3 ComputePerpDirection(Vector3 forwardDir) {
-        return Vector3.Cross(forwardDir, Vector3.up);
+        return Vector3.Cross(forwardDir, Vector3.up).normalized;
     }
 
 
